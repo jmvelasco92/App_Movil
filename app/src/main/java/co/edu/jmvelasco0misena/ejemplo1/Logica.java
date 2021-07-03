ed0a6c83e2803e9f667084eba1140aae11d2b1ca
Victor Martinez <vomartinez@misena.edu.co>
2021-07-02 21:21:29 -0500

    agregar proyecto

---
+++ app/src/main/java/co/edu/jmvelasco0misena/ejemplo1/Logica.java
@@ -0,0 +1,16 @@
+package co.edu.jmvelasco0misena.ejemplo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_logica);
+    }
+
+
+
+}
